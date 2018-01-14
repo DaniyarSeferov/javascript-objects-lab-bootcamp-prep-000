94d4a0c69dd092353ef99687efc2ca48b2067492
Daniyar Seferov <seferovdaniyar@gmail.com>
2018-01-14 08:35:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var recipes = {}
 
 function updateObjectWithKeyAndValue(object, key, value) {
   var newObj = Object.assign({}, object)
-  object[key] = value
-  return object
+  newObj[key] = value
+  return newObj
 }
